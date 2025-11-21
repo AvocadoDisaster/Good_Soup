d9efb5a9b66e071b666a865d7fb6caf26200e6ba
Spaghetti-Doodle <maraspiro8@gmail.com>
2025-11-20 20:43:26 -0500

    moved flowers

--- Assets/Scripts/Logic/MainMenu.cs
+++ Assets/Scripts/Logic/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Milestone1Scene");
+        SceneManager.LoadScene("GameScene");
     }
 
     public void OpenControls()
